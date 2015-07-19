f7a8372090351d8cf84e24731e73da048f6e3030
Dave Ross <dave@davidmichaelross.com>
2015-07-19 12:48:31 -0400

    PHPDocumentor comments & code formatting

--- http2-server-push.php
+++ http2-server-push.php
@@ -9,38 +9,63 @@ Author:      David Michael Ross
 Author URI:  http://davidmichaelross.com
 */
 
+/**
+ * Start an output buffer so this plugin can call header() later without errors
+ *
+ * @param string $template
+ *
+ * @return string
+ */
+function http2_link_template_redirect( $template ) {
 
-add_filter( 'script_loader_src', 'http2_link_script_loader_src', 99, 2 );
+	ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE );
+
+	return $template;
+
+}
+
+add_action( 'template_include', 'http2_link_template_redirect' );
+
+/**
+ * When rendering a <script> tag for an enqueued script, add a Link header signalling the HTTP/2 web server to
+ * push this resource to the client
+ *
+ * @param string $src
+ * @param string $handle
+ *
+ * @return string
+ */
 function http2_link_script_loader_src( $src, $handle ) {
-	global $http2_link_buffer;
+
 	if ( strpos( $src, home_url() ) !== false ) {
 		$relative_src = preg_replace( '/^http(s)?:\/\/[^\/]*/', '', $src );
 		header( 'Link: <' . $relative_src . '>; rel=preload; as=script', false );
 	}
 
 	return $src;
+
 }
 
-add_filter( 'style_loader_src', 'http2_link_style_loader_src', 99, 2 );
+add_filter( 'script_loader_src', 'http2_link_script_loader_src', 99, 2 );
+
+/**
+ * When rendering a <link> tag for an enqueued stylesheet, add a Link header signalling the HTTP/2 web server to
+ * push this resource to the client
+ *
+ * @param string $src
+ * @param string $handle
+ *
+ * @return string
+ */
 function http2_link_style_loader_src( $src, $handle ) {
+
 	if ( strpos( $src, home_url() ) !== false ) {
 		$relative_src = preg_replace( '/^http(s)?:\/\/[^\/]*/', '', $src );
 		header( 'Link: <' . $relative_src . '>; rel=preload; as=stylesheet', false );
 	}
 
 	return $src;
+
 }
 
-add_action( 'wp_head', 'http2_link_wp_head', 999 );
-function http2_link_wp_head() {
-	$matches = array();
-	preg_match( '/\<link(.*)rel="stylesheet"(.*)href="(.*)"/', $http2_link_buffer[0], $matches );
-	// header('Link: <' . $src . '>; rel=preload; as=stylesheet', false);
-}
-
-add_action( 'template_include', 'http2_link_template_redirect' );
-function http2_link_template_redirect( $template ) {
-	ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE );
-
-	return $template;
-}
+add_filter( 'style_loader_src', 'http2_link_style_loader_src', 99, 2 );
