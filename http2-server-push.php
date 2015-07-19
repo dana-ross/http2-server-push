14570fa212e97edf5f3c400f14f705d9e07f9db1
Dave Ross <dave@davidmichaelross.com>
2015-07-19 11:49:31 -0400

    Ripped out a lot of the output buffer craziness

--- http2-server-push.php
+++ http2-server-push.php
@@ -4,42 +4,32 @@
 Plugin Name: http/2 server push 
 */
 
-$http2_link_buffer = array();
-
-ob_start(null, 0, PHP_OUTPUT_HANDLER_CLEANABLE );
-register_shutdown_function(function() {
-  global $http2_link_buffer;
-  echo implode('', $http2_link_buffer);
-});
 add_filter( 'script_loader_src', 'http2_link_script_loader_src', 99, 2);
 function http2_link_script_loader_src($src, $handle) {
   global $http2_link_buffer;
   if (strpos($src, home_url()) !== false) {
-  	$relative_src = preg_replace('/^http(s)?:\/\/[^\/]*/', '', $src);
-    $http2_link_buffer[] = ob_get_clean();
-    ob_start();
+    $relative_src = preg_replace('/^http(s)?:\/\/[^\/]*/', '', $src);
     header('Link: <' . $relative_src . '>; rel=preload; as=script', false);
   }
   return $src;
 }
 add_filter('style_loader_src', 'http2_link_style_loader_src', 99, 2);
 function http2_link_style_loader_src($src, $handle) {
-  global $http2_link_buffer;
   if (strpos($src, home_url()) !== false) {
-  	$relative_src = preg_replace('/^http(s)?:\/\/[^\/]*/', '', $src);
-    $http2_link_buffer[] = ob_get_clean(); 
-    ob_start();
+    $relative_src = preg_replace('/^http(s)?:\/\/[^\/]*/', '', $src);
     header('Link: <' . $relative_src . '>; rel=preload; as=stylesheet', false);
   }
   return $src;
 }
 add_action('wp_head', 'http2_link_wp_head', 999);
 function http2_link_wp_head() {
-    global $http2_link_buffer;
-    $http2_link_buffer[] = ob_get_clean();
-    ob_start();
-    $http2_link_buffer = array(implode('', $http2_link_buffer));
     $matches = array();
     preg_match('/\<link(.*)rel="stylesheet"(.*)href="(.*)"/', $http2_link_buffer[0], $matches);
-    #header('Link: <' . $src . '>; rel=preload; as=stylesheet', false);
+    // header('Link: <' . $src . '>; rel=preload; as=stylesheet', false);
+}
+
+add_action('template_include', 'http2_link_template_redirect');
+function http2_link_template_redirect($template) {
+  ob_start(null, 0, PHP_OUTPUT_HANDLER_CLEANABLE );
+  return $template;
 }
